d2417cb733e525f8f30278c65b39c48c3c3446b4
rxsh96 <rxsh964@gmail.com>
2024-03-03 20:06:48 -0500

    admit null values on hired employees endpoint

--- backend/app/api/endpoints/endpoints.py
+++ backend/app/api/endpoints/endpoints.py
@@ -11,8 +11,8 @@ router = APIRouter()
 def root():
     return {'message': 'Welcome to Globant Challenge!'}
 
-@router.post('/upload-hired_employee/', tags=['Hired Employees'])
-async def upload_hired_employees(file: UploadFile = File(...), db: Session = Depends(get_db)):
+@router.post('/upload-jobs/', tags=['Upload File'])
+async def upload_jobs(file: UploadFile = File(...), db: Session = Depends(get_db)):
     try:
         content = await file.read()
         decoded_content = content.decode('utf-8')
@@ -22,7 +22,56 @@ async def upload_hired_employees(file: UploadFile = File(...), db: Session = Dep
 
         for row in rows:
             print(row)
-            employee = HiredEmployee(id=row[0], name=row[1], datetime=row[2], department_id=row[3], job_id=row[4])
+            job = Job(id=row[0], job=row[1])
+            db.add(job)
+
+        db.commit()
+
+        return {'message': 'CSV file uploaded and data inserted successfully'}
+
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=f'Error: {str(e)}')
+    
+@router.post('/upload-departments/', tags=['Upload File'])
+async def upload_departments(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    try:
+        content = await file.read()
+        decoded_content = content.decode('utf-8')
+
+        csv_reader = csv.reader(decoded_content.splitlines(), delimiter=',')
+        rows = list(csv_reader)
+
+        for row in rows:
+            print(row)
+            department = Department(id=row[0], department=row[1])
+            db.add(department)
+
+        db.commit()
+
+        return {'message': 'CSV file uploaded and data inserted successfully'}
+
+    except Exception as e:
+        print(str(e))
+        raise HTTPException(status_code=500, detail=f'Error: {str(e)}')
+
+
+@router.post('/upload-hired_employee/', tags=['Upload File'])
+async def upload_hired_employees(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    try:
+        content = await file.read()
+        decoded_content = content.decode('utf-8')
+
+        csv_reader = csv.reader(decoded_content.splitlines(), delimiter=',')
+        rows = list(csv_reader)
+        for row in rows:
+            name=row[1] if row[1] != '' else None
+            datetime=row[2] if row[2] != '' else None
+            department_id=row[3] if row[3] != '' else None
+            job_id=row[4] if row[4] != '' else None
+
+            print(row[0], name, datetime, department_id, job_id)
+            employee = HiredEmployee(id=row[0], name=name, datetime=datetime, department_id=department_id, job_id=job_id)
             db.add(employee)
 
         db.commit()
--- backend/app/api/models/models.py
+++ backend/app/api/models/models.py
@@ -7,7 +7,7 @@ class Department(Base):
     __tablename__ = 'departments'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, nullable=False)
+    department = Column(String, index=True, nullable=False)
 
     employees = relationship('HiredEmployee', back_populates='department')
 
@@ -15,7 +15,7 @@ class Job(Base):
     __tablename__ = 'jobs'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, nullable=False)
+    job = Column(String, index=True, nullable=False)
     
     employees = relationship('HiredEmployee', back_populates='job')
 
@@ -23,11 +23,11 @@ class HiredEmployee(Base):
     __tablename__ = 'hired_employees'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, nullable=False)
+    name = Column(String, index=True, nullable=True)
     datetime = Column(String, index=True, nullable=False)
 
-    department_id = Column(Integer, ForeignKey('departments.id'), nullable=False)
-    job_id = Column(Integer, ForeignKey('jobs.id'), nullable=False)
+    department_id = Column(Integer, ForeignKey('departments.id'), nullable=True)
+    job_id = Column(Integer, ForeignKey('jobs.id'), nullable=True)
 
     department = relationship('Department', back_populates='employees')
     job = relationship('Job', back_populates='employees')
