a39e01a348e0f924f2880b2e4875fb8a01c3976b
rxsh96 <rxsh964@gmail.com>
2024-03-04 20:59:03 -0500

    general changes on endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.endpoints.endpoints import upload_hired_employees, root, router
+from app.api.endpoints.endpoints import router
 from app.api.database.base_class import Base
 from app.api.database.database_connection import engine
 
